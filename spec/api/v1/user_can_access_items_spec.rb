d6f995145f27e22a6d82f50fd360cd84dd39fe35
Liam Barstad <liambarstad@gmail.com>
2017-11-01 09:35:45 -0600

    add other CRUD tests for api/items

--- spec/api/v1/user_can_access_items_spec.rb
+++ spec/api/v1/user_can_access_items_spec.rb
@@ -22,6 +22,7 @@ RSpec.feature "user can access items" do
     get('/api/v1/items')
     result = JSON.parse(response.body)
 
+    expect(response.status).to eq(200)
     expect(result.count).to eq(2)
     expect(result[0]["name"]).to eq("sample1")
     expect(result[0]["description"]).to eq("sample1")
@@ -31,14 +32,38 @@ RSpec.feature "user can access items" do
   end
 
   xscenario "and get a single item" do
+    item1 = Item.create(name: "sample1", description: "sample1", image_url: "sample1")
+    item2 = Item.create(name: "sample2", description: "sample2", image_url: "sample2")
 
+    get("/api/v1/items/#{item1.id}")
+    result = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(result["name"]).to eq(item1.name)
+    expect(result["description"]).to eq(item1.description)
+    expect(result["image_url"]).to eq(item1.image_url)
   end
 
-  scenario "and delete an item" do
+  xscenario "and delete an item" do
+    item1 = Item.create(name: "sample1", description: "sample1", image_url: "sample1")
+    item2 = Item.create(name: "sample2", description: "sample2", image_url: "sample2")
 
+    delete("/api/v1/items/#{item1.id}")
+    result = JSON.parse(response.body)
+
+    expect(response.status).to eq(204)
+    expect(Item.first).to eq(item2)
   end
 
-  scenario "and create an item" do
+  xscenario "and create an item" do
+    item1 = Item.new(name: "sample1", description: "sample1", image_url: "sample1")
 
+    post("/api/v1/items")
+    result = JSON.parse(response.body)
+
+    expect(response.status).to eq(204)
+    expect(result["name"]).to eq(item1.name)
+    expect(result["description"]).to eq(item1.description)
+    expect(result["image_url"]).to eq(item1.image_url)
   end
 end
