59e1751ea5d6f5a4848a5080f9a6c097fd8ea307
Liam Barstad <liambarstad@gmail.com>
2017-11-01 11:49:52 -0600

    add pagination to stores/index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,23 @@
 class SearchController < ApplicationController
 
   def index
+    if params[:page] == nil
+      params[:page] = 1
+    end
+
     bbh = BestBuyHandler.new(params[:search])
     search_results = bbh.get_stores
-    @stores = Store.create_from_response(search_results)
+    stores = Store.create_from_response(search_results)
+
+    @results_count = stores.count
+    @page = params[:page]
+    paginate(stores, @page)
+  end
+
+  private
+
+  def paginate(stores, page)
+    @stores = stores[((page - 1) * 10)..((page * 10) - 1)]
   end
 
 end
